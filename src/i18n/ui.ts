5dfa516b0d7ed23c91ab9cdf4398f48d31915fce
emoreno487 <95302300+emoreno487@users.noreply.github.com>
2024-03-22 20:56:15 -0500

    servicios

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -16,6 +16,14 @@ export const ui = {
     "nav.our_team": "Equipo",
     "nav.features": "Características",
     "nav.language": "Lenguaje",
+    "nav.customs": "Aduana",
+    "nav.air_transport": "Transporte Aereo",
+    "nav.marine_transport": "Transporte Maritimo",
+    "nav.ground_transportation": "Transporte Terrestre",
+    "nav.consulting": "Asesorias",
+    "nav.warehouse": "Distribucion y almacenes",
+    "readmore" :"Ver Mas"
+
   },
   en: {
     "nav.home": "Home",
@@ -25,6 +33,13 @@ export const ui = {
     "nav.contact": "Contact",
     "nav.our_team": "Our Team",
     "nav.features": "Features",
-    "nav.language": "language",
+    "nav.language": "Language",
+    "nav.customs": "Aduana",
+    "nav.air_transport": "Air transport",
+    "nav.marine_transport": "Marine Transport",
+    "nav.ground_transportation": "Ground Transportation",
+    "nav.consulting": "Consulting",
+    "nav.warehouse": "Distribution and warehouses",
+    "readmore" :"Read More"
   },
 } as const;
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,7 @@
 export const ASSETS = {
     number: "+57 3162541111",
     name :"SLOC",
-    email : "info@sloc.com",
+    email : "info@slocsas.com",
     address: 'Cartagena /Manga',
     whatsapp: 'https://wa.me/573162541111?text=Hola!%20Estoy%20interesado%20en%20tu%20servicio'
   };
